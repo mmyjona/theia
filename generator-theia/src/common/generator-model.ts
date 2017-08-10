f522643b01fb9760d7426b261592e92171889015
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-08-10 18:25:08 +0200

    [extension-manager] load the project model once a request
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- generator-theia/src/common/generator-model.ts
+++ generator-theia/src/common/generator-model.ts
@@ -5,6 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+// tslint:disable:no-console
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as semver from 'semver';
